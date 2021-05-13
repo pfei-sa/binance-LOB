898407de7a4d118cfdb40a001c00bd87a19edf34
Tony Fei <pfei@uw.edu>
2021-05-13 13:45:05 -0700

    added query for list of snapshot ids

--- replay.py
+++ replay.py
@@ -1,21 +1,19 @@
 from datetime import datetime
-from os import times
-from time import time
-from typing import Dict, Generator, List, Optional, Tuple, final
+from typing import Dict, Generator, List, Optional, Tuple
 from infi.clickhouse_orm.database import Database
-from infi.clickhouse_orm.funcs import F
 from model import DiffDepthStream, DepthSnapshot
 from clickhouse_driver import Client
-import copy
+from config import CONFIG
 
 
 def diff_depth_stream_generator(
-    database: str, last_update_id: int, symbol: str, block_size: Optional[int] = None
+    last_update_id: int, symbol: str, block_size: Optional[int] = None
 ) -> Generator[
     Tuple[datetime, int, int, List[float], List[float], List[float], List[float], str],
     None,
     None,
 ]:
+    database = CONFIG.db_name
     db = Database(database)
     client = Client(host="localhost")
     client.execute(f"USE {database}")
@@ -39,8 +37,9 @@ def diff_depth_stream_generator(
 
 
 def orderbook_generator(
-    database: str, last_update_id: int, symbol: str, block_size: Optional[int] = None
+    last_update_id: int, symbol: str, block_size: Optional[int] = None
 ):
+    database = CONFIG.db_name
     db = Database(database)
     client = Client(host="localhost")
     client.execute(f"USE {database}")
@@ -73,7 +72,7 @@ def orderbook_generator(
     yield (timestamp, last_update_id, bids_book.copy(), asks_book.copy(), symbol)
     prev_final_update_id = None
     for diff_stream in diff_depth_stream_generator(
-        database, last_update_id, symbol, block_size
+        last_update_id, symbol, block_size
     ):
         # https://binance-docs.github.io/apidocs/spot/en/#how-to-manage-a-local-order-book-correctly
         (
@@ -109,10 +108,6 @@ def lists_to_dict(price, quantity):
     return {p: q for p, q in zip(price, quantity)}
 
 
-def dict_filter_zeros(book):
-    return {p: q for p, q in book.items() if q != 0}
-
-
 def update_book(book: Dict, price, quantity):
     for p, q in zip(price, quantity):
         if q == 0:
@@ -121,10 +116,19 @@ def update_book(book: Dict, price, quantity):
             book[p] = q
 
 
+def get_snapshots_update_ids(symbol: str) -> int:
+    database = CONFIG.db_name
+    client = Client(host="localhost")
+    client.execute(f"USE {database}")
+    return client.execute(
+        f"SELECT last_update_id FROM depthsnapshot WHERE symbol = '{symbol.upper()}'"
+    )
+
+
 if __name__ == "__main__":
     i = 0
     first_id = last_id = 0
-    for r in orderbook_generator("archive", 7505673970, "ETHUSDT"):
+    for r in orderbook_generator(7505673970, "ETHUSDT"):
         i += 1
     print(r[2])
     print(i)
