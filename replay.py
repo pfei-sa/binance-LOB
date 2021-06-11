bff7019b533f57e486a7b004b0d06bc9ab624d7c
Tony Fei <pfei@uw.edu>
2021-06-10 22:28:48 -0700

    formatting

--- replay.py
+++ replay.py
@@ -527,7 +527,7 @@ def get_all_symbols() -> List[str]:
     database = CONFIG.db_name
     client = Client(host=CONFIG.host_name)
     client.execute(f"USE {database}")
-    return [s[0] for s in client.execute('SELECT DISTINCT symbol FROM diffdepthstream')]
+    return [s[0] for s in client.execute("SELECT DISTINCT symbol FROM diffdepthstream")]
 
 
 if __name__ == "__main__":
